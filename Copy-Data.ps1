b370677e49ddf448e3098f679476545ad11181fb
Luke Alton <lucas.alton@gmail.com>
2018-02-06 00:08:12 -0700

    Added logic to handle if the Data directory already exists.

--- Copy-Data.ps1
+++ Copy-Data.ps1
@@ -1,2 +1,7 @@
 #This script will copy data from the Data folder to the desktop of the local machine.
-Copy-Item .\Data -Destination C:\Users\Luke\Desktop\Data -Recurse
+if (-Not(Test-Path -Path "C:\Users\Luke\Desktop\Data")) {
+    Copy-Item .\Data -Destination C:\Users\Luke\Desktop\Data -Recurse
+}
+else {
+    Get-Childitem -Path .\Data | Copy-Item -Destination "C:\Users\Luke\Desktop\Data" -Recurse
+}
